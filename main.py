8ad424e4a1fa09ce9148e6c3fa4f99fa15a58e13
PerisAntonio <antonio.perishueso@gmail.com>
2023-10-02 21:17:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def run(input_data, solver_params, extra_arguments):
     logging.basicConfig(level=logging.DEBUG,filename='solver.log',filemode='w',format='%(asctime)s - %(levelname)s - %(message)s')
     logging.warning('This is a warning')
     logging.debug('This is a debug')
-    return 0
+    ret={}
+    ret['Output']='Success'
+    return ret
     
