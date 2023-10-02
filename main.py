594093319ad14fba324700ce03e25ff419d81882
PerisAntonio <antonio.perishueso@gmail.com>
2023-10-02 20:57:58 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import logging
+
+def run(input_data, solver_params, extra_arguments):
+    logging.basicConfig(level=logging.DEBUG,filename='solver.log',filemode='w',format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.warning('This is a warning')
+    logging.debug('This is a debug')
+    return 0
+    
